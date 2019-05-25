4e3ece9e2a23778689be63e05af5953aba8108b4
Christopher Scully <scullycphone@gmail.com>
2019-05-25 12:51:38 -0500

    Set Framing for JS

--- Scripting/Index.js
+++ Scripting/Index.js
@@ -1,3 +1,18 @@
 function tst(){
  alert("test");
 }
+function ratioadj() {
+  var w = window.outerWidth;
+  var h = window.outerHeight;
+  var x = w/h;
+  console.log(x);
+  if (true) {
+      
+  }
+  else if (true) {
+
+  }
+  else {
+
+  }
+}
