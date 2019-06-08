fefa84f21403c9c9cdda49421161a5b56a8a07d3
Christopher Scully <scullycphone@gmail.com>
2019-06-08 11:20:51 -0500

    minor edits/corrections

--- Scripting/Index.js
+++ Scripting/Index.js
@@ -9,12 +9,17 @@ function ratioadj() {
   console.log(x);
   if (x < .90) {
     // document.getElementById("trifold").className = "cardlong";
-    document.getElementsByName("tritle")[0].className = "long";
+    //document.getElementsByName("tritle")[0].id = "lh2";
 
     var Cards = document.getElementsByName("trifold");
 
     for (var i = 0, max = Cards.length; i < max; i++) {
         Cards[i].className = "cardlong";
+    }
+    var Titles = document.getElementsByName("tritle");
+
+    for (var i = 0, max = Titles.length; i < max; i++) {
+        Titles[i].id = "lh2";
     }
     document.getElementsByName("trihold")[0].style.display = "block";
     document.getElementsByName("tritle")[0].style.position = "relative";
@@ -27,6 +32,9 @@ function ratioadj() {
     }
     if(document.getElementById("triR")){
         document.getElementById("triR").id = "LtriB";
+    }
+    if(document.getElementById("quote")){
+        document.getElementById("quote").id = "longquote";
     }
     if(x<.75){
         if(document.getElementById("reg")){
@@ -43,15 +51,30 @@ function ratioadj() {
     }
   else if (x > 2) {
 
+      if(document.getElementById("btitle")){
+          document.getElementById("btitle").id = "reg";
+      }
   }
   else {
+
       var Cards = document.getElementsByName("trifold");
 
       for (var i = 0, max = Cards.length; i < max; i++) {
           Cards[i].className = "card";
       }
+      var Titles = document.getElementsByName("tritle");
+
+      for (var i = 0, max = Titles.length; i < max; i++) {
+          Titles[i].id = "h2";
+      }
       document.getElementsByName("trihold")[0].style.display = "flex";
       document.getElementsByName("tritle")[0].style.position = "absolute";
+
+      if(document.getElementById("btitle")){
+          document.getElementById("btitle").id = "reg";
+      }
+
+      //document.getElementsByName("tritle")[0].id = "h2";
 
       if (document.getElementById("LtriT")) {
           document.getElementById("LtriT").id = "triL";
@@ -62,6 +85,9 @@ function ratioadj() {
       if(document.getElementById("LtriB")){
           document.getElementById("LtriB").id = "triR";
       }
-      document.getElementsByName("tritle")[0].className = "";
+      if(document.getElementById("longquote")){
+          document.getElementById("longquote").id = "quote";
+      }
+      //document.getElementsByName("tritle")[0].className = "";
   }
 }
